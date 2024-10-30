71f542af6296a7a2c652f6c2a6c9044b33e1b6c6
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-10-30 15:34:09 -0400

    started HelloJS

--- JavaScript/HelloJS/HelloJS.js
+++ JavaScript/HelloJS/HelloJS.js
@@ -13,7 +13,8 @@ let b2 = "I am a let! More modern version of var. We typically use these"
 const b3 = "I am a const! My value won't change. Like final in Java"
 
 //Array just for fun - we can use any datatypes we want!
-arr = [4, "some string", true, {objectVal1:5, objectVal2:"something"}, null]
+var arr = [4, "some string", true, {objectVal1:5, objectVal2:"something"}, null]
 //we can use .push() to add stuff to the end of the Array
 arr.push("hello there I'm a new value")
+
 //TODO: we can also access values by index like in Java
