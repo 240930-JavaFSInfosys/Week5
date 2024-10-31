93fd0e249fa8a0c9339b31582ecb95b9759ad3db
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-10-31 13:48:30 -0400

    HelloFS done

--- JavaScript/HelloFunctionsScopes/HelloFS.js
+++ JavaScript/HelloFunctionsScopes/HelloFS.js
@@ -43,3 +43,60 @@ function f2(x, someFunction){
 f2(1000, f1) //f2 is taking in a number and and function f1
 
 console.log("==========================(Scopes)")
+
+//anything that is globally scoped can be accessed anywhere in the script
+console.log(a) //this prints out "undefined"
+
+//This^ is another example of HOISTING - all functions and vars are hoisted
+var a = 5
+
+console.log(a) //a will have a value if it's called after its declaration
+
+
+//Let's try this same thing with a let
+
+//console.log(b) <-THIS IS ILLEGAL! lets and consts are NOT HOISTED
+//This is one reason we use let instead of var now - we don't want to access variables before their declaration
+
+let b = 5
+
+console.log(b) //This is fine of course. b has been declared at this point
+
+console.log("==========================(Local Scope (function vs block scope))")
+
+//block scope----------------/
+
+//any variable within a block that ISN'T a function
+
+if(true){
+    var c = "I am a var in a block - block scoped!"
+    console.log(c)
+    let d = "I am a let in a block - block scoped!"
+    console.log(d)
+}
+
+console.log(c) //vars are globally scoped so they're visible OUTSIDE their block scope
+//console.log(d) //lets and consts are NOT VISIBLE outside their block scope
+
+
+//function scope---------------/
+
+//any variable defined in a function is function scoped
+function scopeTester(){
+    var e = "I am a var in a function - function scoped!"
+    console.log(e)
+}
+
+scopeTester()
+
+console.log(e) //FUNCTIONS are the only blocks that confine vars to their scope
+
+/* The main difference between block and function scope is:
+
+    -vars are NOT visible outside the function they're declared in
+    -BUT... vars ARE visible outside any non-function block they're declared in
+    
+    -lets and consts are only visible within their block, period.
+
+    Since we only really use let and const these days, we don't have to worry about these quirks
+    (but you should still know them for QC) */
