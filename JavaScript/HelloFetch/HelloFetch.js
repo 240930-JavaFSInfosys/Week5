df1bd3b1de241c6263c71c6c1f475662ad36d703
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-11-01 10:30:11 -0400

    setup for fetch request

--- JavaScript/HelloFetch/HelloFetch.js
+++ JavaScript/HelloFetch/HelloFetch.js
@@ -10,3 +10,23 @@ let pokepic = document.getElementById("pokepic");
 //when the user clicks the button, execute the fetchData method (using an eventListener)
 document.getElementById("btn").onclick = fetchData;
 
+/* This function will return some Pokemon data from PokeAPI 
+It will use fetch request to return a promise object 
+The promise object will get filled with the data requests, OR it will fail*/
+async function fetchData(){
+
+    //First, we need to gather the user's input from the input box
+    let userInput = document.getElementById("userInput").value
+    //.value will extract whatever value is in the input box 
+
+    console.log(userInput) //just to see the value
+
+    //Then, we can use the fetch() method to send a GET request to pokeAPI
+    //fetch sends GET requets by default BTW
+    await fetch($URL + userInput) //https://...../pokemon/{whatever the user put in}
+    .then()
+    .then()
+    .catch()
+    .finally()
+
+}
