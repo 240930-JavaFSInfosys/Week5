214cae1cf18958a1a8e97e1c6c84649b83bd3cd3
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-11-04 15:25:30 -0500

    showed 2 examples of conditional rendering

--- React/hello-events-routing/src/Components/CalculationComponent/Calculation.tsx
+++ React/hello-events-routing/src/Components/CalculationComponent/Calculation.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import "./Calculation.css"
 
 export const CalculationComponent:React.FC = () => {
 
@@ -8,23 +9,58 @@ export const CalculationComponent:React.FC = () => {
     const [sideB, setSideB] = useState<number>(0)
     const [result, setResult] = useState<number>(0)
 
-    //TODO: function to store the side values
-    const storeValues = () => {
+    //This function will store the SideA or SideB input values (depending on which one changed)
+    //We'll take the name of the input, and assign that value to the appropriate state variable
+    const storeValues = (input:any) => {
         
+        //If the name of the changed input is "SideA", when store it in the sideA state variable
+        //Otherwise, store the input in the sideB state variable
+        if(input.target.name === "SideA"){
+            setSideA(input.target.value)
+        } else {
+            setSideB(input.target.value)
+        }
+
+        //"input" is going to be the entire event object, which stores our target
+        //so what is "target"? it refers to the specific element that was triggered (one of our input boxes)
+
     }
 
-    //TODO: function to do the calculation and render it
+    //Function to do the calculation and store it in the result state variable
     const calculate = () => {
+        
+        //TODO: user input handling
+
+        //We can use the Math class to calculate Hypotenuse
+        let hypontenuse = Math.hypot(sideA, sideB)
+
+        //set the result state!
+        setResult(hypontenuse)
+
+        //yes, this could have just been one line :)
 
     }
 
     return(
         <div className="input-container">
-            
+
             <h3>Calculation Component</h3>
 
             <input type="number" name="SideA" onChange={storeValues}/>
             <input type="number" name="SideB" onChange={storeValues}/>
+
+            {/* Conditional Rendering! If SideA or SideB are <= 0, render an error message */}
+            <p style={{color:"red"}}>
+                {/* "Are sideA and sideB both > 0? good. otherwise, render an error message" */}
+                {sideA > 0 && sideB > 0 ? "":"please enter positive integers for both sides"}
+            </p>
+
+            <div>
+                {/* "if result is truthy (nonzero), tell the user their result. otherwise render nothing" */}
+                {result ? <p>You calculated: {result}</p>:""}
+            </div>
+
+            <button onClick={calculate}>Calculate Hypotenuse!</button>
 
         </div>
     )
