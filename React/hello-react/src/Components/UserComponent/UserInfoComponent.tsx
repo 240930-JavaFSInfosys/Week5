458327c6c36c4af1a5153bc1421c3b71cc71062e
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-11-04 10:34:40 -0500

    almost done with hello-react, talked about useEffect

---
+++ React/hello-react/src/Components/UserComponent/UserInfoComponent.tsx
@@ -0,0 +1,16 @@
+//This component will display user data 
+
+import { UserInterface } from "../../Interfaces/UserInterface"
+
+//It takes props of UserInterface type, so it only accepts UserInterface objects from a parent
+export const UserInfoComponent:React.FC<UserInterface> = (user:UserInterface) => {
+
+    
+
+    return(
+        <div>
+
+        </div>
+    )
+
+}
